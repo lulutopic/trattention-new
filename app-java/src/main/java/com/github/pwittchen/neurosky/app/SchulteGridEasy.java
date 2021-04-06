713957c95f34ed33780bee3bcecce1efa354e378
arielwang98 <73053707+arielwang98@users.noreply.github.com>
2021-04-06 15:53:31 +0800

    舒爾特加手勢bug版

--- app-java/src/main/java/com/github/pwittchen/neurosky/app/SchulteGridEasy.java
+++ app-java/src/main/java/com/github/pwittchen/neurosky/app/SchulteGridEasy.java
@@ -5,8 +5,10 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.os.SystemClock;
 import android.text.Layout;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,11 +20,270 @@ import android.widget.TextView;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import java.util.Arrays;
 import java.util.Collections;
 
+import com.madgaze.watchsdk.MGWatch;
+import com.madgaze.watchsdk.MobileActivity;
+import com.madgaze.watchsdk.WatchException;
+import com.madgaze.watchsdk.WatchGesture;
 
-public class SchulteGridEasy extends AppCompatActivity {
+
+public class SchulteGridEasy extends MobileActivity {
+    private final String TAG = MainActivity.class.getSimpleName();
+
+    public final WatchGesture[] REQUIRED_WATCH_GESTURES = {
+            //彈指
+            WatchGesture.FINGER_SNAP,
+            //手臂
+            WatchGesture.FOREARM_LEFT,
+            WatchGesture.FOREARM_RIGHT,
+            //手背
+            WatchGesture.HANDBACK_UP,
+            WatchGesture.HANDBACK_DOWN,
+            WatchGesture.HANDBACK_LEFT,
+            WatchGesture.HANDBACK_RIGHT,
+            WatchGesture.MOVE_FOREARM_DOWN,
+            //拇指中指捏捏
+            WatchGesture.THUMBTAP_MIDDLE,
+            //三指捏捏
+            WatchGesture.THUMBTAP_INDEX_MIDDLE,
+            //拇指食指捏捏
+            WatchGesture.THUMBTAP_INDEX,
+            //指頭
+            WatchGesture.JOINTTAP_LOWER_THUMB,
+            WatchGesture.JOINTTAP_UPPER_THUMB,
+            WatchGesture.JOINTTAP_MIDDLE_INDEX,
+            WatchGesture.JOINTTAP_UPPER_INDEX,
+            WatchGesture.JOINTTAP_MIDDLE_MIDDLE,
+            WatchGesture.JOINTTAP_UPPER_MIDDLE,
+            WatchGesture.JOINTTAP_MIDDLE_RING,
+            WatchGesture.JOINTTAP_UPPER_RING,
+            WatchGesture.JOINTTAP_MIDDLE_LITTLE,
+            //手臂快速移動
+            WatchGesture.MOVE_FOREARM_DOWN,
+            WatchGesture.MOVE_FOREARM_LEFT,
+            WatchGesture.MOVE_FOREARM_UP,
+            WatchGesture.MOVE_FOREARM_RIGHT,
+    };
+
+
+
+
+    @Override
+    public void onWatchGestureReceived(WatchGesture gesture) {
+        Log.d(TAG, "onWatchGestureReceived: "+gesture.name());
+        setResultText(gesture);
+    }
+
+    @Override
+    public void onWatchGestureError(WatchException error) {
+        Log.d(TAG, "onWatchGestureError: "+error.getMessage());
+        setStatusText(error.getMessage());
+    }
+
+    @Override
+    public void onWatchDetectionOn() {
+        Log.d(TAG, "onWatchDetectionOn: ");
+        setStatusText("Listening");
+    }
+
+    @Override
+    public void onWatchDetectionOff() {
+        Log.d(TAG, "onWatchDetectionOff: ");
+        setStatusText("Idle");
+    }
+
+    @Override
+    public void onMGWatchServiceReady() {
+        setStatusText("Service Connected");
+        tryStartDetection();
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+
+        if (MGWatch.isWatchGestureDetecting(this))
+            MGWatch.stopGestureDetection(this);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        if (MGWatch.isMGWatchServiceReady(this))
+            tryStartDetection();
+    }
+
+    @Override
+    public void onWatchConnected() {
+        setStatusText("Watch Connected");
+    }
+
+    @Override
+    public void onWatchDisconnected() {
+        setStatusText("Watch Disconnected");
+        showConnectDialog();
+    }
+
+    @Override
+    protected WatchGesture[] getRequiredWatchGestures(){
+        return REQUIRED_WATCH_GESTURES;
+    }
+
+    private void tryStartDetection(){
+
+        if (!MGWatch.isWatchConnected(this)) {
+            setStatusText("Connecting");
+            showConnectDialog();
+            return;
+        }
+
+        if (!MGWatch.isGesturesTrained(this)) {
+            showTrainingDialog();
+            return;
+        }
+
+        if (!MGWatch.isWatchGestureDetecting(this)) {
+            MGWatch.startGestureDetection(this);
+        }
+    }
+
+
+    private void setStatusText(String text){
+        setText(R.id.status, "Status: " + text);
+    }
+
+    private void setResultText(final WatchGesture gesture){
+        setText(R.id.result, gesture.toString());
+        ImageView[] UnShuffle = {one,two,three,four,five,six,seven,eight,nine,ten,eleven,twelve,thirteen,fourteen,fifteen,sixteen};
+
+
+        if(gesture == WatchGesture.HANDBACK_DOWN){
+            clearRow(focus_row);
+            focus_row+=1;
+            switch(focus_row){
+                case(2):
+                    row2.setBackgroundColor(focus_color);
+                    break;
+                case(3):
+                    row3.setBackgroundColor(focus_color);
+                    break;
+                case(4):
+                    row4.setBackgroundColor(focus_color);
+                    break;
+                case(5):
+                    focus_row=1;
+                    row1.setBackgroundColor(focus_color);
+                    break;
+            }
+        }
+        else if(gesture == WatchGesture.FOREARM_RIGHT){
+            clearColumn(focus_column);
+            focus_column+=1;
+            switch(focus_column){
+                case(2):
+                    UnShuffle[1].setBackgroundColor(focus_color);
+                    UnShuffle[5].setBackgroundColor(focus_color);
+                    UnShuffle[9].setBackgroundColor(focus_color);
+                    UnShuffle[13].setBackgroundColor(focus_color);
+                    break;
+                case(3):
+                    UnShuffle[2].setBackgroundColor(focus_color);
+                    UnShuffle[6].setBackgroundColor(focus_color);
+                    UnShuffle[10].setBackgroundColor(focus_color);
+                    UnShuffle[14].setBackgroundColor(focus_color);
+                    break;
+                case(4):
+                    UnShuffle[3].setBackgroundColor(focus_color);
+                    UnShuffle[7].setBackgroundColor(focus_color);
+                    UnShuffle[11].setBackgroundColor(focus_color);
+                    UnShuffle[15].setBackgroundColor(focus_color);
+                    break;
+                case(5):
+                    focus_column=1;
+                    UnShuffle[0].setBackgroundColor(focus_color);
+                    UnShuffle[4].setBackgroundColor(focus_color);
+                    UnShuffle[8].setBackgroundColor(focus_color);
+                    UnShuffle[12].setBackgroundColor(focus_color);
+                    break;
+            }
+        }
+        else if(gesture == WatchGesture.THUMBTAP_INDEX){
+            focus_count=(focus_row-1)*4+focus_column-1;
+            int theCard = Integer.parseInt((String)UnShuffle[focus_count].getTag());
+            doStuff(UnShuffle[focus_count],theCard);
+        }
+    }
+
+    public void setDefinedGestures(){
+        setText(R.id.definedGestures, TextUtils.join(", ", getRequiredWatchGestures()));
+    }
+
+    public void showConnectDialog(){
+        android.app.AlertDialog.Builder dialog = new android.app.AlertDialog.Builder(this);
+        dialog.setTitle("Connect Required")
+                .setMessage("Watch is not connected. Connect to MAD Gaze Watch now.")
+                .setPositiveButton("Connect", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        MGWatch.connect(SchulteGridEasy.this);
+                    }
+                })
+                .setCancelable(false);
+        dialog.show();
+    }
+
+    public void showTrainingDialog(){
+        android.app.AlertDialog.Builder dialog = new android.app.AlertDialog.Builder(this);
+        dialog.setTitle("Training Required")
+                .setMessage("The required gestures for this application have not been trained. Do you want to train now?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        MGWatch.trainRequiredGestures(SchulteGridEasy.this);
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        setStatusText("Training Required");
+                        ((Button)findViewById(R.id.trainButton)).setVisibility(View.VISIBLE);
+                        dialog.dismiss();
+                    }
+                });
+        dialog.show();
+    }
+
+    public void setListeners(){
+        ((Button)findViewById(R.id.trainButton)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setVisibility(View.GONE);
+                MGWatch.trainRequiredGestures(SchulteGridEasy.this);
+            }
+        });
+    }
+
+    public void setText(final int resId, final String text){
+        if (Looper.myLooper() == Looper.getMainLooper()) {
+            TextView textView = ((TextView) findViewById(resId));
+            if (textView != null)
+                textView.setText(text);
+        } else runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                TextView textView = ((TextView)findViewById(resId));
+                if (textView != null)
+                    textView.setText(text);
+            }
+        });
+    }
+
+
+
+
     private Long startTime; //初始時間
     private Long spentTime;
     private Long pauseTime=0L;
@@ -32,6 +293,7 @@ public class SchulteGridEasy extends AppCompatActivity {
     private int focus_count;
     private int focus_row=1;
     private int focus_column=1;
+
 
 
     //圖片的變數
